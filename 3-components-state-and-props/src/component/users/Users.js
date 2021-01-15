2e1ad13870c5a4eabc661d0328db8c1f562daec3
Matthijs Boet <m.boet@chello.nl>
2021-01-16 00:53:29 +0100

    finished section 3

--- 3-components-state-and-props/src/component/users/Users.js
+++ 3-components-state-and-props/src/component/users/Users.js
@@ -1,56 +1,26 @@
-import React, { Component } from 'react'
-import UserItem from './UserItem'
+import React from 'react'
+import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types'
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        login: "mojombo",
-        id: 1,
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        login: "defunkt",
-        id: 2,
-        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/defunkt",
-      },
-      {
-        login: "pjhyett",
-        id: 3,
-        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/pjhyett",
-      },
-      {
-        login: "wycats",
-        id: 4,
-        avatar_url: "https://avatars0.githubusercontent.com/u/4?v=4",
-        html_url: "https://github.com/wycats",
-      },
-      {
-        login: "ezmobius",
-        id: 5,
-        avatar_url: "https://avatars0.githubusercontent.com/u/5?v=4",
-        html_url: "https://github.com/ezmobius",
-      },
-      {
-        login: "ivey",
-        id: 6,
-        avatar_url: "https://avatars0.githubusercontent.com/u/6?v=4",
-        html_url: "https://github.com/ivey",
-      },
-    ],
-  };
-  render() {
+
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return Spinner
+  } else {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
-          <UserItem key={user.id} user={user}/>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
-    )
+    );
   }
+}
+
+Users.protoTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
 }
 
 const userStyle = {
